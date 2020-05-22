f8821d036ae11088d7f4808c152f4491dab179ef
angusaddlesee <angus@addlesee.co.uk>
2020-05-22 17:56:22 +0100

    added msoft results storage

---
+++ asr-msoft/results/batch1/README.md
@@ -0,0 +1,3 @@
+# Results Storage
+
+Results from Google will be stored here. Add structure here to mirror `/data` structure.
--- asr-msoft/run.sh
+++ asr-msoft/run.sh
@@ -1,3 +1,3 @@
 docker rm $(docker ps -a -q)
 docker build -t asr/msoft .
-docker run --name azure -v /path/to/incremental-asr-evaluation/data/batch1/:/data -v /path/to/incremental-asr-evaluation/asr-msoft/results/:/results asr/msoft
+docker run --name azure -v /path/to/incremental-asr-evaluation/data/batch1/:/data -v /path/to/incremental-asr-evaluation/asr-msoft/results/batch1/:/results asr/msoft
